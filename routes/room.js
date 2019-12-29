248de38f61cfd54a786bdae828126fb0b7101f3e
gilmarxd <gilmar_custodio_@hotmail.com>
2019-12-29 14:33:02 -0300

    minor changes

--- routes/room.js
+++ routes/room.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('room', { title: 'Express' });
+router.get('/:id', function(req, res, next) {
+  res.render('room', { title: `Gartico - Room ${req.params.id}` });
 });
 
 module.exports = router;
