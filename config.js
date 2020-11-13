58b107ae7c1fe1c33f8723a36f5f1379340b2506
Gilmar <gilmar_custodio_@hotmail.com>
2020-11-13 06:51:32 -0300

    set env vars in config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 var config = {
-    host: 'localhost',
-    port: 3000,
-    mysql_user: 'root',
-    mysql_password: '123456',
-    mysql_database: 'gartico'
-}
+  host: process.env.HOST || "localhost",
+  port: process.env.PORT || 3000,
+  sql_user: process.env.SQL_USER,
+  sql_password: process.env.SQL_PASSWORD,
+  sql_database: process.env.SQL_DATABASE,
+};
 
-module.exports = config
+module.exports = config;
